9d02f5ad590befabbcec0ad67c1633bc147ae653
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-02 09:41:09 +0200

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -35,55 +35,11 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    if not await db.is_user_exist(message.chat.id):
-        await db.add_user(message.chat.id)
-    ## Force Sub ##
-    if UPDATES_CHANNEL is not None:
-        try:
-            user = await client.get_chat_member(UPDATES_CHANNEL, message.chat.id)
-            if user.status == "kicked":
-               await message.reply_text(
-                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
-                   parse_mode="markdown",
-                   disable_web_page_preview=True
-               )
-               return
-        except UserNotParticipant:
-            await message.reply_text(
-                text="**Please Join My Updates Channel to use this Bot!**",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
-                        ]
-                    ]
-                ),
-                parse_mode="markdown"
-            )
-            return
-        except Exception:
-            await message.reply_text(
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
-                parse_mode="markdown",
-                disable_web_page_preview=True
-            )
-            return
-    ## Force Sub ##
+    
+   
     await message.reply_text(
         Localisation.HELP_MESSAGE,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
-                ],
-                [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
-                ],
-                [
-                    InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
-                    InlineKeyboardButton('Source Code', url='https://github.com/AbirHasan2005/VideoCompress') # Must Give us Credits!
-                ]
-            ]
+     
         ),
         quote=True
     )
